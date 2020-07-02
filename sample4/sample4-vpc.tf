a239dfc1ad7f4ecadac944d5ae6e5280b91fa214
Koichi Taura <taura.koichi@gmail.com>
2020-07-02 18:26:49 +0900

    add route table and association

--- sample4/sample4-vpc.tf
+++ sample4/sample4-vpc.tf
@@ -46,5 +46,85 @@ resource "aws_subnet" "db" {
   }
 }
 
+resource "aws_route_table" "web" {
+  vpc_id = aws_vpc.vpc.id
+  tags = {
+    Name = "web"
+  }
+}
 
+resource "aws_route_table" "app" {
+  vpc_id = aws_vpc.vpc.id
+  tags = {
+    Name = "app"
+  }
+}
+
+resource "aws_route_table" "db" {
+  vpc_id = aws_vpc.vpc.id
+  tags = {
+    Name = "db"
+  }
+}
+
+resource "aws_route_table_association" "web" {
+  count = length(var.aws_availability_zones[var.aws_region])
+  subnet_id      = aws_subnet.web[count.index].id
+  route_table_id = aws_route_table.web.id
+}
+
+resource "aws_route_table_association" "app" {
+  count = length(var.aws_availability_zones[var.aws_region])
+  subnet_id      = aws_subnet.app[count.index].id
+  route_table_id = aws_route_table.app.id
+}
+
+resource "aws_route_table_association" "db" {
+  count = length(var.aws_availability_zones[var.aws_region])
+  subnet_id      = aws_subnet.db[count.index].id
+  route_table_id = aws_route_table.db.id
+}
+
+resource "aws_internet_gateway" "igw" {
+  vpc_id = aws_vpc.vpc.id
+  tags = {
+    Name = "sample4-igw"
+  }
+}
+
+resource "aws_eip" "nat_gateway" {
+  vpc        = true
+  depends_on = [aws_internet_gateway.igw]
+  tags = {
+    Name    = "sample4-nat-eip"
+  }
+}
+
+resource "aws_nat_gateway" "nat_gateway" {
+  allocation_id = aws_eip.nat_gateway.id
+  subnet_id     = aws_subnet.web[0].id
+  depends_on    = [aws_internet_gateway.igw]
+
+  tags = {
+    Name    = "sample4-nat-gw"
+  }
+}
+
+resource "aws_route" "web" {
+  route_table_id         = aws_route_table.web.id
+  gateway_id             = aws_internet_gateway.igw.id
+  destination_cidr_block = "0.0.0.0/0"
+}
+
+resource "aws_route" "app" {
+  route_table_id         = aws_route_table.app.id
+  nat_gateway_id         = aws_nat_gateway.nat_gateway.id
+  destination_cidr_block = "0.0.0.0/0"
+}
+
+resource "aws_route" "db" {
+  route_table_id         = aws_route_table.db.id
+  nat_gateway_id         = aws_nat_gateway.nat_gateway.id
+  destination_cidr_block = "0.0.0.0/0"
+}
 
